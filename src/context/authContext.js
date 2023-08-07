f010513abb86147ce84ed88646eb2726777ad386
pmanonline <petersonzoconis@gmail.com>
2023-08-07 17:40:54 +0100

    added session storage

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user")) || null
+    JSON.parse(sessionStorage.getItem("user")) || null
   );
 
   const login = async (inputs) => {
@@ -29,7 +29,7 @@ export const AuthContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(currentUser));
+    sessionStorage.setItem("user", JSON.stringify(currentUser));
   }, [currentUser]);
 
   return (
